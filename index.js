9c0762352c8b2a98fc5f2f3100bf5699aa8bca7b
unknown <gabriel_121297@hotmail.com>
2023-10-07 22:01:20 -0300

    add prod

--- index.js
+++ index.js
@@ -23,14 +23,15 @@ app.get("/", async (req, res) => {
 });
 
 // OK
-app.get("/resumo", async (req, res) => {
+app.post("/resumo", async (req, res) => {
+  7;
+  console.log(req.body);
   req.body.prompt = "string";
 
   if (typeof req.body.prompt === "string") {
     const response = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt:
-        "Olá, ChatGPT! Se comporte como um um professor. Gostaria de solicitar sua ajuda para criar resumos de disciplinas escolares abordadas pelos professores. Isso permitirá que os alunos utilizem o resumo como base ou alternativa ao que foi ensinado em sala de aula, preparando-os para discussões posteriores e atividades relacionadas ao tema. Por favor, crie um resumo conciso sobre o tópico de HISTORIA - FASCISMO. O objetivo é fornecer uma visão geral do assunto, abrangendo as principais informações de forma clara e sucinta, incluindo exemplos práticos relacionados ao tópico para facilitar a compreensão.",
+      prompt: `Olá, ChatGPT! Se comporte como um um professor. Gostaria de solicitar sua ajuda para criar resumos de disciplinas escolares abordadas pelos professores. Isso permitirá que os alunos utilizem o resumo como base ou alternativa ao que foi ensinado em sala de aula, preparando-os para discussões posteriores e atividades relacionadas ao tema. Por favor, crie um resumo conciso sobre o tópico de ${req.body.subject} - ${req.body.theme}. O objetivo é fornecer uma visão geral do assunto, abrangendo as principais informações de forma clara e sucinta, incluindo exemplos práticos relacionados ao tópico para facilitar a compreensão.`,
       temperature: 0.4,
       max_tokens: 2048,
     });
@@ -42,14 +43,13 @@ app.get("/resumo", async (req, res) => {
 });
 
 // OK
-app.get("/caracteristica", async (req, res) => {
+app.post("/caracteristica", async (req, res) => {
   req.body.prompt = "string";
 
   if (typeof req.body.prompt === "string") {
     const response = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt:
-        "Olá, comporte-se como um especialista no tema HISTORIA - FASCISMO, e liste os principais caracteristicas sobre o tema. Seja o mais detalhista possível e cite exemplos práticos.",
+      prompt: `olá, comporte-se como um especialista no tema ${req.body.subject} - ${req.body.theme}, e liste os principais caracteristicas sobre o tema. Seja o mais detalhista possível e cite exemplos práticos.`,
       temperature: 0.4,
       max_tokens: 2048,
     });
@@ -61,14 +61,13 @@ app.get("/caracteristica", async (req, res) => {
 });
 
 // OK
-app.get("/causa", async (req, res) => {
+app.post("/causa", async (req, res) => {
   req.body.prompt = "string";
 
   if (typeof req.body.prompt === "string") {
     const response = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt:
-        "Olá, comporte-se como um especialista em HISTORIA - FASCISMO, e liste os principais causas sobre ele. Seja o mais detalhista possível e cite exemplos práticos.",
+      prompt: `olá, comporte-se como um especialista em ${req.body.subject} - ${req.body.theme}, e liste os principais causas sobre ele. Seja o mais detalhista possível e cite exemplos práticos.`,
       temperature: 0.4,
       max_tokens: 2048,
     });
@@ -79,14 +78,13 @@ app.get("/causa", async (req, res) => {
 });
 
 // OK
-app.get("/cronograma-aluno", async (req, res) => {
+app.post("/cronograma-aluno", async (req, res) => {
   req.body.prompt = "string";
 
   if (typeof req.body.prompt === "string") {
     const response = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt:
-        "Olá Chat,Gostaria de criar um material de estudo, que será um cronograma destinado a um estudante. Neste cronograma deve conter os temas que o aluno deve estudar para que o assunto principal que foi solicitado seja mais completamente abordado levando em consideração a quantidade de dias e horas por dia que o aluno vai passar estudando.Tema: FASCISMO, Dias: 3,  Horas por dia: 2 horas .A resposta deve vir em forma de lista, separando os dias em tópicos e o tema abordado neste respectivo dia em subtópicos. Caso seja em apenas um dia, apresente somente os subtópicos.Também deve estar claro quanto tempo o aluno deve separar para cada subtópico.",
+      prompt: `olá Chat,Gostaria de criar um material de estudo, que será um cronograma destinado a um estudante. Neste cronograma deve conter os temas que o aluno deve estudar para que o assunto principal que foi solicitado seja mais completamente abordado levando em consideração a quantidade de dias e horas por dia que o aluno vai passar estudando.Tema: ${req.body.theme}, Dias: ${req.body.days},  Horas por dia: ${req.body.hours} horas .A resposta deve vir em forma de lista, separando os dias em tópicos e o tema abordado neste respectivo dia em subtópicos. Caso seja em apenas um dia, apresente somente os subtópicos..`,
       temperature: 0.4,
       max_tokens: 2048,
     });
@@ -97,14 +95,13 @@ app.get("/cronograma-aluno", async (req, res) => {
 });
 
 // OK
-app.get("/cronograma-professor", async (req, res) => {
+app.post("/cronograma-professor", async (req, res) => {
   req.body.prompt = "string";
 
   if (typeof req.body.prompt === "string") {
     const response = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt:
-        "Olá, ChatGPT! se comporte como um especialista em criar roteiro de aulas. Levando em consideração a matéria, tema a ser abordado, tempo de aula. Aqui estão as informações necessárias :Matéria: HISTÓRIA, Tema: FASCISMO. Estrutura do roteiro: O roteiro deve ser composto por entre 4 e 8 tópicos principais. Cada tópico principal deve ser acompanhado por 1 a 4 subtópicos. Quanto mais relevante e extenso for o tema, mais subtópicos podem ser incluídos.",
+      prompt: `olá, ChatGPT! se comporte como um especialista em criar roteiro de aulas. Levando em consideração a matéria, tema a ser abordado, aulas que o professor terá para abordar o assunto. Aqui estão as informações necessárias :Matéria: ${req.body.subject}, Tema: ${req.body.theme}, Quantidade de aulas para abordar o tema: ${req.body.days}. Estrutura do roteiro: O roteiro deve ser detalhado de forma eficaz para o professor entender o que ele deverá abordar em cada dia de aula. Cada tópico principal deve ser acompanhado por 1 a 4 subtópicos. Quanto mais relevante e extenso for o tema, mais subtópicos podem ser incluídos.`,
       temperature: 0.4,
       max_tokens: 2048,
     });
@@ -115,14 +112,13 @@ app.get("/cronograma-professor", async (req, res) => {
 });
 
 // OK
-app.get("/exercicio-professor", async (req, res) => {
+app.post("/exercicio-professor", async (req, res) => {
   req.body.prompt = "string";
 
   if (typeof req.body.prompt === "string") {
     const response = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt:
-        "Olá chat ! Comporte-se como um professor que está criando exercícios de fixação para seus alunos. Você deve criar exercícios de nível médio para os alunos, levando em consideração os temas e tópicos abordados na aula. Os exercícios devem ser de múltipla escolha, tendo cada um entre 4 e 5 opções e somente uma correta. Ao lado de cada questão deve conter o gabarito para que o professor possa corrigir mais facilmente, GERE O MAXIMO DE QUESTOES POSSIVEIS. Tema: Tópico: HISTORIA - FASCISMO",
+      prompt: `olá chat ! Comporte-se como um professor que está criando exercícios de fixação para seus alunos. Você deve criar exercícios de nível médio para os alunos, levando em consideração os temas e tópicos abordados na aula. Os exercícios devem ser de múltipla escolha, tendo cada um entre 4 e 5 opções e somente uma correta. Ao lado de cada questão deve conter o gabarito para que o professor possa corrigir mais facilmente, GERE O MAXIMO DE QUESTOES POSSIVEIS. Tema: Tópico: ${req.body.subject} - ${req.body.theme}`,
       temperature: 0.4,
       max_tokens: 2048,
     });
@@ -133,14 +129,13 @@ app.get("/exercicio-professor", async (req, res) => {
 });
 
 // OK
-app.get("/exercicio-aluno", async (req, res) => {
+app.post("/exercicio-aluno", async (req, res) => {
   req.body.prompt = "string";
 
   if (typeof req.body.prompt === "string") {
     const response = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt:
-        "Olá chat ! Comporte-se como um professor que está criando exercícios de fixação para seus alunos. Você deve criar exercícios de nível médio para os alunos, levando em consideração os temas e tópicos abordados na aula. Os exercícios devem ser de múltipla escolha, tendo cada um entre 4 e 5 opções e somente uma correta, lembre de não não informar qual a resposta correta. GERE O MAXIMO DE QUESTOES POSSIVEIS. Tema: Tópico: HISTORIA - FASCISMO",
+      prompt: `olá chat ! Comporte-se como um professor que está criando exercícios de fixação para seus alunos. Você deve criar exercícios de nível médio para os alunos, levando em consideração os temas e tópicos abordados na aula. Os exercícios devem ser de múltipla escolha, tendo cada um entre 4 e 5 opções e somente uma correta, lembre de não não informar qual a resposta correta. GERE O MAXIMO DE QUESTOES POSSIVEIS. Tema: Tópico: ${req.body.subject} - ${req.body.theme}`,
       temperature: 0.4,
       max_tokens: 2048,
     });
@@ -151,14 +146,13 @@ app.get("/exercicio-aluno", async (req, res) => {
 });
 
 // OK
-app.get("/consequencia", async (req, res) => {
+app.post("/consequencia", async (req, res) => {
   req.body.prompt = "string";
 
   if (typeof req.body.prompt === "string") {
     const response = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt:
-        "Olá, comporte-se como um especialista em HISTORIA - FASCISMO, e liste os principais consequencias sobre ele. Seja o mais detalhista possível e cite exemplos práticos.",
+      prompt: `olá, comporte-se como um especialista em ${req.body.subject} - ${req.body.theme}, e liste os principais consequencias sobre ele. Seja o mais detalhista possível e cite exemplos práticos.`,
       temperature: 0.4,
       max_tokens: 2048,
     });
@@ -171,14 +165,13 @@ app.get("/consequencia", async (req, res) => {
 });
 
 // OK
-app.get("/formulas-exatas", async (req, res) => {
+app.post("/formulas-exatas", async (req, res) => {
   req.body.prompt = "string";
 
   if (typeof req.body.prompt === "string") {
     const response = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt:
-        "Olá chat. Se comporte como uma ótima fonte para obter fórmulas e regras relacionadas a matérias exatas da escola, como matemática, física, química, geometria e biologia. Liste as principais fórmulas relacionadas a disciplina e tema informados abaixo, as elencando em tópicos, além de uma breve descrição quanto a suas finalidades reais. Disciplina: Matemática Tema: Equação de segundo grau",
+      prompt: `olá chat. Se comporte como uma ótima fonte para obter fórmulas e regras relacionadas a matérias exatas da escola, como matemática, física, química, geometria e biologia. Liste as principais fórmulas relacionadas a disciplina e tema informados abaixo, as elencando em tópicos, além de uma breve descrição quanto a suas finalidades reais. Disciplina: Matemática Tema: Equação de segundo grau`,
       temperature: 0.4,
       max_tokens: 2048,
     });
@@ -191,14 +184,13 @@ app.get("/formulas-exatas", async (req, res) => {
 });
 
 // OK
-app.get("/fonte-estudos", async (req, res) => {
+app.post("/fonte-estudos", async (req, res) => {
   req.body.prompt = "string";
 
   if (typeof req.body.prompt === "string") {
     const response = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt:
-        "Olá chat. Se comporte como uma ótima referência para indicar fontes de estudo. Gostaria de obter boas fontes de estudo no idioma português sobre os temas mais relevantes, referente a disciplina e tema descritos abaixo. As fontes serão usadas como base para que alunos possam estudar melhor.  Disciplina: História  Tema: Fascismo",
+      prompt: `olá chat. Se comporte como uma ótima referência para indicar fontes de estudo. Gostaria de obter boas fontes de estudo no idioma português sobre os temas mais relevantes, referente a disciplina e tema descritos abaixo. As fontes serão usadas como base para que alunos possam estudar melhor.  Disciplina: ${req.body.subject}  Tema: ${req.body.theme}`,
       temperature: 0.4,
       max_tokens: 2048,
     });
@@ -211,14 +203,13 @@ app.get("/fonte-estudos", async (req, res) => {
 });
 
 // OK
-app.get("/ludico", async (req, res) => {
+app.post("/ludico", async (req, res) => {
   req.body.prompt = "string";
 
   if (typeof req.body.prompt === "string") {
     const response = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt:
-        "me ensine sobre FASCISMO como se fosse um filme de ficção cientifica, me conte uma longa história",
+      prompt: `me ensine sobre ${req.body.theme} da disciplina ${req.body.subject} como se fosse ${req.body.ludic}, me conte uma longa história`,
       temperature: 0.4,
       max_tokens: 2048,
     });
@@ -231,14 +222,13 @@ app.get("/ludico", async (req, res) => {
 });
 
 // OK
-app.get("/curiosidade", async (req, res) => {
+app.post("/curiosidade", async (req, res) => {
   req.body.prompt = "string";
 
   if (typeof req.body.prompt === "string") {
     const response = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt:
-        "Olá chat. Se comporte como uma enciclopédia de curiosidades relacionadas a temas aleatórios. Os temas aleatórios serão descritos posteriormente neste prompt, levando em consideração a disciplina e tema. Porém você deve me listar 3, 4 ou 5 curiosidades relacionadas ao tema solicitado. As curiosidades devem ser listadas em tópicos e conter um breve resumo. Disciplina: História Tema: Fascismo",
+      prompt: `olá chat. Se comporte como uma enciclopédia de curiosidades relacionadas a temas aleatórios. Os temas aleatórios serão descritos posteriormente neste prompt, levando em consideração a disciplina e tema. Porém você deve me listar 3, 4 ou 5 curiosidades relacionadas ao tema solicitado. As curiosidades devem ser listadas em tópicos e conter um breve resumo. Disciplina: ${req.body.subject} Tema: ${req.body.theme}`,
       temperature: 0.4,
       max_tokens: 2048,
     });
@@ -251,14 +241,13 @@ app.get("/curiosidade", async (req, res) => {
 });
 
 // OK
-app.get("/relato", async (req, res) => {
+app.post("/relato", async (req, res) => {
   req.body.prompt = "string";
 
   if (typeof req.body.prompt === "string") {
     const response = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt:
-        "Olá chat. Se comporte com uma ótima fonte de relatos de pessoas reais, sobre tópicos aleatórios. Os tópicos serão descritos posteriormente neste prompt, levando em consideração a disciplina e tema solicitado. Os relatos devem conter falas de figuras relevantes referente ao tema e disciplina solicitados. Disciplina: Historia Tema: Segunda guerra mundial",
+      prompt: `olá chat. Se comporte com uma ótima fonte de relatos de pessoas reais, sobre tópicos aleatórios. Os tópicos serão descritos posteriormente neste prompt, levando em consideração a disciplina e tema solicitado. Os relatos devem conter falas de figuras relevantes referente ao tema e disciplina solicitados. Disciplina: ${req.body.subject} Tema: Segunda guerra mundial`,
       temperature: 0.4,
       max_tokens: 2048,
     });
@@ -271,14 +260,13 @@ app.get("/relato", async (req, res) => {
 });
 
 // OK
-app.get("/topicos", async (req, res) => {
+app.post("/topicos", async (req, res) => {
   req.body.prompt = "string";
 
   if (typeof req.body.prompt === "string") {
     const response = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt:
-        "Olá, comporte-se como um especialista em HISTORIA - FASCISMO, e liste os principais tópicos sobre ele. Liste o maior número de tópicos possível.",
+      prompt: `olá, comporte-se como um especialista em ${req.body.subject} - ${req.body.theme}, e liste os principais tópicos sobre ele. Liste o maior número de tópicos possível.`,
       temperature: 0.4,
       max_tokens: 2048,
     });
@@ -306,7 +294,7 @@ app.post("/resposta", async (req, res) => {
   }
 });
 
-app.get("/imagewm", async (req, res) => {
+app.post("/imagewm", async (req, res) => {
   req.body.prompt = "string";
   if (typeof req.body.prompt === "string") {
     const response = await openai.createImage({
@@ -332,7 +320,7 @@ app.post("/chat", async (req, res) => {
     "Espanhol.",
     "Artes.",
     "Educação Física.",
-    "História.",
+    "${req.body.subject}.",
     "Geografia.",
     "Sociologia.",
     "Filosofia.",
@@ -347,7 +335,7 @@ app.post("/chat", async (req, res) => {
       model: "text-davinci-003",
       prompt:
         req.body.prompt +
-        "-essa pergunta tem a ver com alguma dessas disciplinas? Língua Portuguesa, Literatura, Inglês, Espanhol, Artes, Educação Física, História, Geografia, Sociologia, Filosofia,  Biologia, Química ,Física, Matemática. se tiver, responda em única palavra qual seria a disciplinas com um ponto final ao dizer qual disciplinas. Caso não tenha, responda que não tem a ver com nenhuma disciplina.",
+        `-essa pergunta tem a ver com alguma dessas disciplinas? Língua Portuguesa, Literatura, Inglês, Espanhol, Artes, Educação Física, ${req.body.subject}, Geografia, Sociologia, Filosofia,  Biologia, Química ,Física, Matemática. se tiver, responda em única palavra qual seria a disciplinas com um ponto final ao dizer qual disciplinas. Caso não tenha, responda que não tem a ver com nenhuma disciplina.`,
       temperature: 0,
       max_tokens: 2048,
     });
